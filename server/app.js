787fd81724bb927b7cea1a2247b9d2d7a703d690
Scott Ashmore <dev.scottashmore@gmail.com>
2016-03-10 11:11:03 +0000

    implementing clusters

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 require('dotenv').config({ silent: true });
 
+var cluster    = require('cluster');
+var numCPUs    = require('os').cpus().length;
 var express    = require('express');
 var app        = express();
 var server     = require('http').Server(app);
@@ -11,48 +13,67 @@ var AppRouter  = require('./router');
 var timeKeeper = require('./timeKeeper');
 var port       = process.env.PORT || 3000;
 
-server.listen(port);
+if (cluster.isMaster) {
 
-io.on('connection', function(socket) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
 
-  // Sockets.init(io, socket);
+    cluster.fork();
 
-  var newConnection = Object.create(Sockets);
+  }
 
-  newConnection.init(io, socket);
+  cluster.on('exit', (worker, code, signal) => {
 
-});
+    console.log(`worker ${worker.process.pid} died`);
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+  });
 
-mongoose.connect(process.env.MONGOLAB_URI);
+} else {
 
-// Routes for our api
-var router = express.Router();
+  server.listen(port);
 
-// middleware to use for all requests
-router.use(function(req, res, next) {
+  io.on('connection', function(socket) {
 
-  // make sure we go to the next routes and don't stop here
-  next();
+    // Sockets.init(io, socket);
 
-});
+    var newConnection = Object.create(Sockets);
 
-AppRouter.home(router.route('/'));
-AppRouter.createUser(router.route('/users'));
-AppRouter.getUsers(router.route('/users'));
-AppRouter.getUser(router.route('/users/:id'));
-AppRouter.updateUser(router.route('/users/:id'));
-AppRouter.removeUser(router.route('/users/:id'));
-AppRouter.createRace(router.route('/races'));
-AppRouter.getRaces(router.route('/races'));
-AppRouter.getRace(router.route('/races/:id'));
-AppRouter.updateRace(router.route('/races/:id'));
+    newConnection.init(io, socket);
 
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+  });
 
-setInterval(timeKeeper, 1000);
+  // configure app to use bodyParser()
+  // this will let us get the data from a POST
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  mongoose.connect(process.env.MONGOLAB_URI);
+
+  // Routes for our api
+  var router = express.Router();
+
+  // middleware to use for all requests
+  router.use(function(req, res, next) {
+
+    // make sure we go to the next routes and don't stop here
+    next();
+
+  });
+
+  AppRouter.home(router.route('/'));
+  AppRouter.createUser(router.route('/users'));
+  AppRouter.getUsers(router.route('/users'));
+  AppRouter.getUser(router.route('/users/:id'));
+  AppRouter.updateUser(router.route('/users/:id'));
+  AppRouter.removeUser(router.route('/users/:id'));
+  AppRouter.createRace(router.route('/races'));
+  AppRouter.getRaces(router.route('/races'));
+  AppRouter.getRace(router.route('/races/:id'));
+  AppRouter.updateRace(router.route('/races/:id'));
+
+  // all of our routes will be prefixed with /api
+  app.use('/api', router);
+
+  setInterval(timeKeeper, 1000);
+
+}
