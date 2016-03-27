6bd692e2aca8dcf6bd0fefe00b8fee25af380f1a
Scott Ashmore <sashmore@hi-res.net>
2016-03-27 13:26:06 +0100

    fix auth name

--- server/firebase.js
+++ server/firebase.js
@@ -3,7 +3,7 @@ var FireBaseTokenGen = require('firebase-token-generator');
 
 var Races    = new Firebase('https://real-time-running.firebaseio.com/races');
 var tokenGen = new FireBaseTokenGen(process.env.FIREBASE_SECRET);
-var uid      = { uid: 'realTimeRunningAuth' };
+var uid      = { uid: 'realTimeRunningServer' };
 var token    = tokenGen.createToken(uid);
 
 Races.authWithCustomToken(token, function(error) {
